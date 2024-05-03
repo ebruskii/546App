0cd25fc5d4981fb55ef0bf14e55367b11f98e7ba
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2024-05-03 12:02:12 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 // Purpose: to export all the routes to the server
 import dashboardRoutes from "./dashboard.js";
 import usersRoutes from "./users.js";
-//import profileRoutes from "./profile.js";
+import profileRoutes from "./profile.js";
 
 const constructorMethod = (app) => {
   app.use("/", usersRoutes);
