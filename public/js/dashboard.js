35101b8024e71e0638aeca05f2f9b415d0f825cc
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2024-05-03 12:13:19 -0400

    added second modal

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,20 @@
 let workoutbutton = document.getElementById("addWorkout");
+let modalType = document.getElementById("addWorkoutModalType");
 let modal = document.getElementById("addWorkoutModal");
-workoutbutton.addEventListener("click", async (e) => {
-    modal.style.display = "block";
+let findWorkout = document.getElementById("findWorkout");
+workoutbutton.addEventListener("click", async (event) => {
+    modalType.style.display = "block";
 
+});
+findWorkout.addEventListener("click", async (event) => {
+    event.preventDefault();
+    modalType.style.display = "none";
+    let workoutType = document.getElementById("workoutType").value;
+    let workoutForm = document.getElementById("workoutForm");
+    //continue making the form on js that will populate as a modal
+
+
+    
+
+    modal.style.display = "block";
 });
