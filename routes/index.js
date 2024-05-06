b40ce7f4bf91ca07c881b158ac5aa9c60688465d
Elizabeth Bruski <113567164+ebruskii@users.noreply.github.com>
2024-05-05 20:54:16 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,12 @@
 import dashboardRoutes from "./dashboard.js";
 import usersRoutes from "./users.js";
 import profileRoutes from "./profile.js";
-
+// import challengesRoutes from "./challenges.js";
 const constructorMethod = (app) => {
   app.use("/", usersRoutes);
   app.use("/dashboard", dashboardRoutes);
   app.use("/profile", profileRoutes);
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> 554d496028f4baf54998f1e023980b6d297e224d
+  // app.use("/challenges", challengesRoutes);
   app.use("*", (req, res) => {
     res.sendStatus(404);
   });
